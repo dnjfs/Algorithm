364dda904de130a9b33b75f09c34533f6a34464c
정호진 <48848466+dnjfs@users.noreply.github.com>
2020-03-13 21:08:52 +0900

    2020.03.13 금
    https://www.acmicpc.net/problem/11497
    
    N개의 통나무를 원형으로 세웠을 때 인접한 통나무 간의 높이 차의 최댓값인 난이도가 최소가 되게 만드는 문제입니다.
    첫번째 통나무와 마지막 통나무가 인접해 있으므로 단순히 정렬을 하면 안되고 배열의 양쪽 가장자리부터 번갈아서 채우면 됩니다.
    level을 계산할때 첫번째 통나무와 마지막 통나무의 높이 차를 계산하지 않는 이유는 어차피 높이 차의 최댓값을 구해야하는 거므로 첫번째와 두번째 통나무, 또는 마지막과 마지막 전의 통나무의 높이 차가 첫번째와 마지막 통나무의 높이 차보다 무조건 크거나 같기 때문입니다.

---
+++ ACM-ICPC/11497_통나무_건너뛰기.cpp
@@ -0,0 +1,44 @@
+//ACM-ICPC 2015 본선 E번 통나무 건너뛰기
+//백준 11497
+
+#include "iostream"
+#include "algorithm"
+
+using namespace std;
+
+int main()
+{
+	int T; //테스트 케이스
+	cin >>T;
+	while(T--)
+	{
+		int N, L[10000], circle[10000], level = 0;
+		/*
+		N: 통나무의 개수
+		L[]: 통나무의 높이
+		circle[]: 원형으로 세운 통나무
+		level: 난이도
+		*/
+
+		cin >>N;
+		for(int i = 0; i < N; i++)
+			cin >>L[i];
+		sort(L, L+N); //오름차순 정렬
+
+		int idx = 0, left = 0, right = N-1; //L[] 탐색 인덱스, circle의 왼쪽, 오른쪽 인덱스
+		while(left <= right)
+		{
+			circle[left++] = L[idx++];
+			if(idx < N) //N이 홀수면 마지막에 넣으면 안됨
+				circle[right--] = L[idx++];
+		}
+
+		for(int i = 1; i < N; i++) //level 계산
+		{
+			int t = abs(circle[i]-circle[i-1]);
+			if(level < t)
+				level = t;
+		}
+		cout <<level <<endl;
+	}
+}
